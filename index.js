4c9497a36feeda0070cce0674f59ac9c29da539e
benspector-mls <ben@marcylabschool.org>
2022-09-29 17:38:35 +0000

    finished the fiveToOneHundred function and tested

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 //Code your solutions in this file
 function fiveToOneHundred(){
-    
+    // console.log from 5 to 100
+    for (let i = 5; i <= 100; i++) {
+        console.log(i);
+    }
 }
+console.log("Testing fiveToOneHundred");
+fiveToOneHundred();
